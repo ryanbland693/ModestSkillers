18b6ab1184faaabf5348abc30f8db4356d78cfe0
ryanband693 <ryanbland693@gmail.com>
2024-06-24 19:40:50 +0200

    created/implemented initial API

---
+++ WebAPI/Controllers/RankController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RankChecker.BusinessLogic;
+
+namespace WebAPI.Controllers
+{
+    [Route("/api/rank")]
+    public class RankController : ControllerBase
+    {
+        private readonly IRankService rankService;
+
+        public RankController(IRankService rankService)
+        {
+            this.rankService = rankService;
+        }
+        [HttpGet("rankups")]
+        public async Task<ActionResult<List<ClanMemberRankCheck>>> GetRankupsAsync([FromQuery] bool checkTooHigh, [FromQuery] bool includeUnknownJoinDates)
+        {
+            var rankups = await rankService.CheckRanksAsync(checkTooHigh, includeUnknownJoinDates);
+            if (rankups is not null)
+            {
+                return Ok(rankups);
+            }
+            else
+            {
+                return BadRequest("Something went wrong");
+            }
+        }
+        
+    }
+}
